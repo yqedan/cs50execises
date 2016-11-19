b0b56fb5b089f31a5aee3645eee4cfc09edd0ccf
Yusuf Qedan <yusuf9191@gmail.com>
2016-11-19 19:33:42 +0000

    add comments to initials

--- pset2/initials.c
+++ pset2/initials.c
@@ -5,18 +5,18 @@
 int main(void){
     string x = GetString();
     for(int i = 0 ; i < strlen(x) ; i++){
-        if(i == 0){
-            if((char) x[0] < 97){
-                printf("%c", x[0]);
-            }else{
+        if(i == 0){ // am I the first character?
+            if((char) x[0] < 97){ //am I upper case?
+                printf("%c", x[0]);//print
+            }else{ //convert it to upper case and print
                 printf("%c", (char) (( (int) x[0] ) - 32));
             }
         }
-        if((int) x[i] == 32){
-            if(i+1 < strlen(x)){
-                if((char) x[i+1] < 97){
-                    printf("%c", x[i+1]);
-                }else{
+        if((int) x[i] == 32){ // am I a space ?
+            if(i+1 < strlen(x)){ // If I am look ahead to the next char if possible
+                if((char) x[i+1] < 97){ //am I upper case?
+                    printf("%c", x[i+1]); //print
+                }else{ //convert it to upper case and print
                     printf("%c", (char) (( (int) x[i+1] ) - 32));
                 }
             }
