7056e0b57a1ffce99a05aec570b1ecbebad8e7d2
Yusuf Qedan <yusuf9191@gmail.com>
2016-11-19 06:45:34 +0000

    add ability to crack passwords up to 4 digits long

--- pset2/crack.c
+++ pset2/crack.c
@@ -36,14 +36,154 @@ int main(int argc, string argv[]){
         }
         guess = crypt(words[i],salt);
         if(strcmp(encPwd,guess) == 0){
-            found = 1;
+            found = 2;
             break;
         }
         i++;
     }
+    
+    char a;
+    char test[9];
+    //1 character
+    if(found == 0){
+        for(int j = 0;j < 95;j++){
+            a = ((char) (32 + j));
+            test[0] = a;
+            guess = crypt(test,salt);
+            if(strcmp(encPwd,guess) == 0){
+                found = 1;
+                break;
+            }
+        }
+    }
+    
+    //2 characters
+    if(found == 0){
+        for(int j = 0;j < 95;j++){
+            a = ((char) (32 + j));
+            test[0] = a;
+            for(int k = 0;k < 95;k++){
+                a = ((char) (32 + k));
+                test[1] = a;
+                guess = crypt(test,salt);
+                if(strcmp(encPwd,guess) == 0){
+                    found = 1;
+                    break;
+                }
+            }
+            if(found == 1){
+                break;
+            }
+        }
+    }
+    
+    //3 characters
+    if(found == 0){
+        for(int j = 0;j < 95;j++){
+            a = ((char) (32 + j));
+            test[0] = a;
+            for(int k = 0;k < 95;k++){
+                a = ((char) (32 + k));
+                test[1] = a;
+                for(int l = 0;l < 95;l++){
+                    a = ((char) (32 + l));
+                    test[2] = a;
+                    guess = crypt(test,salt);
+                    if(strcmp(encPwd,guess) == 0){
+                        found = 1;
+                        break;
+                    }
+                }
+                if(found == 1){
+                    break;
+                }
+            }
+            if(found == 1){
+                break;
+            }
+        }
+    }
+    
+    //4 characters
+    if(found == 0){
+        for(int j = 0;j < 95;j++){
+            a = ((char) (32 + j));
+            test[0] = a;
+            for(int k = 0;k < 95;k++){
+                a = ((char) (32 + k));
+                test[1] = a;
+                for(int l = 0;l < 95;l++){
+                    a = ((char) (32 + l));
+                    test[2] = a;
+                    for(int m = 0;m < 95;m++){
+                        a = ((char) (32 + m));
+                        test[3] = a;
+                        guess = crypt(test,salt);
+                        if(strcmp(encPwd,guess) == 0){
+                            found = 1;
+                            break;
+                        }
+                    }
+                    if(found == 1){
+                        break;
+                    }
+                }
+                if(found == 1){
+                    break;
+                }
+            }
+            if(found == 1){
+                break;
+            }
+        }
+    }
+    
+    //5 characters
+    // if(found == 0){
+    //     for(int j = 0;j < 95;j++){
+    //         a = ((char) (32 + j));
+    //         test[0] = a;
+    //         for(int k = 0;k < 95;k++){
+    //             a = ((char) (32 + k));
+    //             test[1] = a;
+    //             for(int l = 0;l < 95;l++){
+    //                 a = ((char) (32 + l));
+    //                 test[2] = a;
+    //                 for(int m = 0;m < 95;m++){
+    //                     a = ((char) (32 + m));
+    //                     test[3] = a;
+    //                     for(int n = 0;n < 95;n++){
+    //                         a = ((char) (32 + n));
+    //                         test[4] = a;
+    //                         guess = crypt(test,salt);
+    //                         if(strcmp(encPwd,guess) == 0){
+    //                             found = 1;
+    //                             break;
+    //                         }
+    //                     }
+    //                     if(found == 1){
+    //                         break;
+    //                     }
+    //                 }
+    //                 if(found == 1){
+    //                     break;
+    //                 }
+    //             }
+    //             if(found == 1){
+    //                 break;
+    //             }
+    //         }
+    //         if(found == 1){
+    //             break;
+    //         }
+    //     }
+    // }
+    
     if(found == 1){
+        printf("%s\n",test);
+    }else if(found == 2){
         printf("%s",words[i]);
-    }else{
+    }else if (found == 0){
         printf("Password is too strong\n");
     }
 }
